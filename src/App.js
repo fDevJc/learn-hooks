3e46773d5ca84376ba4aed0f72434f0a490f22bc
fDevJc <fDevJc@gmail.com>
2021-02-28 17:30:21 +0900

    make useTab by using useState hook

--- src/App.js
+++ src/App.js
@@ -21,6 +21,27 @@ const useInput = (initialValue, validator) => {
   return { value, onChange };
 };
 
+const sections = [
+  {
+    key: 1,
+    tab: 'section 1',
+    content: 'section 1 content',
+  },
+  {
+    key: 2,
+    tab: 'section 2',
+    content: 'section 2 content',
+  },
+];
+
+const useTab = (sequence) => {
+  const [curIdx, setIdx] = useState(0);
+  return {
+    section: sequence[curIdx],
+    changeContent: setIdx,
+  };
+};
+
 const App = () => {
   const maxLen = (value) => {
     return value.length <= 10;
@@ -28,6 +49,9 @@ const App = () => {
 
   const name = useInput('Mr.', maxLen);
   const [item, setItem] = useState(0);
+
+  const { section, changeContent } = useTab(sections);
+
   const incrementItem = () => {
     setItem(item + 1);
   };
@@ -36,12 +60,20 @@ const App = () => {
   };
   return (
     <>
+      <h3>useState</h3>
       <div>{item}</div>
       <button onClick={incrementItem}>increment</button>
       <br></br>
       <button onClick={decrementItem}>decrement</button>
-      <br></br>
+      <h3>useInput</h3>
       <input placeholder="what' your name" {...name}></input>
+      <h3>useContent</h3>
+      {sections.map((section, index) => (
+        <button key={section.key} onClick={() => changeContent(index)}>
+          {section.tab}
+        </button>
+      ))}
+      <div>{section.content}</div>
     </>
   );
 };

a9d5f347871914d569a07fb15a933a6517454884
fDevJc <fDevJc@gmail.com>
2021-02-28 16:02:16 +0900

    to make useInput by unsing hook

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # learn-react-hooks
 
 learn react's hooks
+
+## useState
+
+```javascipt
+
+const [item, setItem] = useState(0);
+
+const incrementItem = () => {
+    setItem(item+1);
+}
+
+```
--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setValue(value);
+  };
+  return { value, onChange };
+};
+
 const App = () => {
+  const name = useInput('Mr.');
   const [item, setItem] = useState(0);
   const incrementItem = () => {
     setItem(item + 1);
@@ -15,6 +27,8 @@ const App = () => {
       <button onClick={incrementItem}>increment</button>
       <br></br>
       <button onClick={decrementItem}>decrement</button>
+      <br></br>
+      <input placeholder="what' your name" {...name}></input>
     </>
   );
 };
